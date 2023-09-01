0bf6b1c31f5bdcdc33b71efc3c33ad41a145b931
Bianca Viccari <viccarybianca@gmail.com>
2023-09-01 17:10:52 +0200

    feat: cena cumpleaños

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import "./style.css";
 
 console.log("Hello Typescript!");
+
+/* 6 personas. Precio cena: 120€. Precio bebidas 18€.¿Cuánto paga cada uno?*/
+
+const valorTicket = 120;
+const precioBebidas = 18;
+const numeroPersonas = 6;
+console.log("Dinero por persona: ", (valorTicket-precioBebidas)/numeroPersonas);
