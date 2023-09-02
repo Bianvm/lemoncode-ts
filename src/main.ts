f5aeffff2d00d218013f8b20d88c02eff8b5d1d5
Bianca Viccari <viccarybianca@gmail.com>
2023-09-02 09:49:48 +0200

    Interfaz de grupos musicales TS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,44 @@
-import "./style.css";
+//Interfaz grupo musical//
 
-console.log("Hello Typescript!");
+interface MusicalBand {
+    name: string;
+    ageFoundation:number;
+    active:boolean;
+    musicalGenre: string;
+};
 
-/* 6 personas. Precio cena: 120€. Precio bebidas 18€.¿Cuánto paga cada uno?*/
-
-const valorTicket = 120;
-const precioBebidas = 18;
-const numeroPersonas = 6;
-console.log("Dinero por persona: ", (valorTicket-precioBebidas)/numeroPersonas);
+const beatles: MusicalBand={
+    name: "The Beatles",
+    ageFoundation:1960,
+    active:false,
+    musicalGenre: "Pop Rock",
+};
+const queen: MusicalBand={
+    name: "Queen",
+    ageFoundation:1970,
+    active:false,
+    musicalGenre: "Rock",
+};
+const acdc: MusicalBand={
+    name: "ACDC",
+    ageFoundation:1973,
+    active:true,
+    musicalGenre: "Hard Rock",
+};
+const ludwigVanBeethoven: MusicalBand={
+    name: "Ludwig van Beethoven",
+    ageFoundation:1770,
+    active:false,
+    musicalGenre: "Classical music",
+};
+const theRollingStones: MusicalBand={
+    name: "The Rolling Stones",
+    ageFoundation:1962,
+    active:true,
+    musicalGenre: "Rock",
+};
+console.log(`%c${beatles.name}`,"font-size:24px; background:green; font-weidth:bold;" ,`/ ${beatles.ageFoundation} `+`/activo: ${beatles.active}`+`/ ${beatles.musicalGenre}`);
+console.log(`%c${queen.name}`,"font-size:24px; background:green; font-weidth:bold;" ,`/ ${queen.ageFoundation} `+`/activo: ${queen.active}`+`/ ${queen.musicalGenre}`);
+console.log(`%c${acdc.name}`,"font-size:24px; background:green; font-weidth:bold;" ,`/ ${acdc.ageFoundation} `+`/activo: ${acdc.active}`+`/ ${acdc.musicalGenre}`);
+console.log(`%c${ludwigVanBeethoven.name}`,"font-size:24px; background:green; font-weidth:bold;" ,`/ ${ludwigVanBeethoven.ageFoundation} `+`/activo: ${ludwigVanBeethoven.active}`+`/ ${ludwigVanBeethoven.musicalGenre}`);
+console.log(`%c${theRollingStones.name}`,"font-size:24px; background:green; font-weidth:bold;" ,`/ ${theRollingStones.ageFoundation} `+`/activo: ${theRollingStones.active}`+`/ ${theRollingStones.musicalGenre}`);
