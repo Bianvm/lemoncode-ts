4027c3729eb006ca7990f8f70da2fe95a1aa1f1f
Bianca Viccari <viccarybianca@gmail.com>
2023-09-08 11:56:36 +0200

    Tarea diplay turno

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,57 @@
-import "./style.css";
+const shiftCounter = document.getElementById('shiftCounter');
+const boxText = document.getElementById('box-text');
 
-console.log("Hello Typescript!");
+const buttonPrevious= document.getElementById('previousshift');
+buttonPrevious?.addEventListener("click", previous);
+
+const buttonNext= document.getElementById('nextshift');
+buttonNext?.addEventListener("click", next);
+
+const buttonReset= document.getElementById('resetshift');
+buttonReset?.addEventListener("click", reset);
+
+const operatorButton=document.getElementById('operatorbutton');
+operatorButton?.addEventListener("click", operator);
+
+
+function previous (){
+    if (!!shiftCounter){
+        let currentShift=parseInt(shiftCounter.innerHTML);
+        if (currentShift>0){
+            currentShift--;
+            shiftCounter.innerHTML=currentShift.toString().padStart(2, '0');
+            
+        }
+    }
+}
+function next (){
+    if (!!shiftCounter){
+        let currentShift=parseInt(shiftCounter.innerHTML);
+        if (currentShift>=0 && currentShift<99){
+            currentShift++;
+            shiftCounter.innerHTML=currentShift.toString().padStart(2, '0');
+            
+        }
+
+    }
+}
+function reset (){
+    if (!!shiftCounter){
+        let currentShift=parseInt(shiftCounter.innerHTML);
+        if (currentShift>0){
+            currentShift=0;
+            shiftCounter.innerHTML=currentShift.toString().padStart(2, '0');
+            
+        }
+    }
+}
+
+function operator (){
+    let operatorShift = parseInt((shiftCounter as HTMLInputElement).value);
+        if (!!boxText && !!shiftCounter && operatorShift<100 && operatorShift>=0){
+            shiftCounter.innerHTML = operatorShift.toString().padStart(2, '0');
+        } else{
+            alert('Por favor, introduzca un número válido del 1 al 99');
+        }
+        
+}           
