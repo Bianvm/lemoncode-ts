1658f3e8bc53d330fdd4d5c423e769460f00f97f
Bianca Viccari <viccarybianca@gmail.com>
2023-09-13 11:07:36 +0200

    corrección ejercicio modulo4

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,65 @@
-const shiftCounter = document.getElementById('shiftCounter');
-const boxText = document.getElementById('box-text');
+const shiftCounter = document.getElementById('shiftCounter') as HTMLHeadingElement;
+const boxText = document.getElementById('box-text') as HTMLInputElement;
 
-const buttonPrevious= document.getElementById('previousshift');
-buttonPrevious?.addEventListener("click", previous);
+const buttonPrevious = document.getElementById('previousshift');
+if (buttonPrevious !== null && buttonPrevious !== undefined && buttonPrevious instanceof HTMLButtonElement) {
+    buttonPrevious.addEventListener("click", previous);
+}
 
-const buttonNext= document.getElementById('nextshift');
-buttonNext?.addEventListener("click", next);
+const buttonNext = document.getElementById('nextshift');
+if (buttonNext !== null && buttonNext !== undefined && buttonNext instanceof HTMLButtonElement) {
+    buttonNext.addEventListener("click", next);
+}
 
-const buttonReset= document.getElementById('resetshift');
-buttonReset?.addEventListener("click", reset);
+const buttonReset = document.getElementById('resetshift');
+if (buttonReset !== null && buttonReset !== undefined && buttonReset instanceof HTMLButtonElement) {
+    buttonReset.addEventListener("click", reset);
+}
 
-const operatorButton=document.getElementById('operatorbutton');
-operatorButton?.addEventListener("click", operator);
+const operatorButton = document.getElementById('operatorbutton');
+if (operatorButton !== null && operatorButton !== undefined && operatorButton instanceof HTMLButtonElement) {
+    operatorButton.addEventListener("click", operator);
+}
 
 
-function previous (){
-    if (!!shiftCounter){
-        let currentShift=parseInt(shiftCounter.innerHTML);
-        if (currentShift>0){
+function previous() {
+    if (shiftCounter !== null && shiftCounter !== undefined && shiftCounter instanceof HTMLHeadingElement) {
+        let currentShift = parseInt(shiftCounter.innerHTML);
+        if (currentShift > 0) {
             currentShift--;
-            shiftCounter.innerHTML=currentShift.toString().padStart(2, '0');
-            
+            shiftCounter.textContent = currentShift.toString().padStart(2, '0');
+
         }
     }
 }
-function next (){
-    if (!!shiftCounter){
-        let currentShift=parseInt(shiftCounter.innerHTML);
-        if (currentShift>=0 && currentShift<99){
+function next() {
+    if (shiftCounter !== null && shiftCounter !== undefined && shiftCounter instanceof HTMLHeadingElement) {
+        let currentShift = parseInt(shiftCounter.innerHTML);
+        if (currentShift >= 0 && currentShift < 99) {
             currentShift++;
-            shiftCounter.innerHTML=currentShift.toString().padStart(2, '0');
-            
+            shiftCounter.textContent = currentShift.toString().padStart(2, '0');
+
         }
 
     }
 }
-function reset (){
-    if (!!shiftCounter){
-        let currentShift=parseInt(shiftCounter.innerHTML);
-        if (currentShift>0){
-            currentShift=0;
-            shiftCounter.innerHTML=currentShift.toString().padStart(2, '0');
-            
+function reset() {
+    if (shiftCounter !== null && shiftCounter !== undefined && shiftCounter instanceof HTMLHeadingElement) {
+        let currentShift = parseInt(shiftCounter.innerHTML);
+        if (currentShift > 0) {
+            currentShift = 0;
+            shiftCounter.textContent = currentShift.toString().padStart(2, '0');
+
         }
     }
 }
 
-function operator (){
+function operator() {
     let operatorShift = parseInt((boxText as HTMLInputElement).value);
-        if (!!boxText && !!shiftCounter && operatorShift<100 && operatorShift>=0){
-            shiftCounter.innerHTML = operatorShift.toString().padStart(2, '0');
-        } else{
-            alert('Por favor, introduzca un número válido del 1 al 99');
-        }
-        
+    if (!!boxText && !!shiftCounter && operatorShift < 100 && operatorShift >= 0) {
+        shiftCounter.textContent = operatorShift.toString().padStart(2, '0');
+    } else {
+        alert('Por favor, introduzca un número válido del 1 al 99');
+    }
+
 }           
