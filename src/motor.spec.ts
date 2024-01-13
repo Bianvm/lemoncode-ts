907a548c34f4f88e651d880617dea4b084962b70
Bianca Viccari <viccarybianca@gmail.com>
2024-01-13 19:53:35 +0100

    test unitario

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -7,19 +7,19 @@ import {
 import { describe, vi } from 'vitest';
 describe('./src/motor.ts', () => {
     describe('obtenerEstadoPartida', () => {
-        it("debería devolver POR_DEBAJO_MAXIMO puntuación < 7.5", () => {
+        it("debería devolver POR_DEBAJO_MAXIMO si puntuación < 7.5", () => {
             const estadoEsperado: EstadoPartida = 'POR_DEBAJO_MAXIMO';
             vi.spyOn(partida, 'puntuacion', "get").mockReturnValue(1);
             const resultado = obtenerEstadoPartida()
             expect(resultado).toBe(estadoEsperado);
         })
-        it("debería devolver TE_HAS_PASADO puntuación > 7.5", () => {
+        it("debería devolver TE_HAS_PASADO si puntuación > 7.5", () => {
             const estadoEsperado: EstadoPartida = 'TE_HAS_PASADO';
             vi.spyOn(partida, 'puntuacion', "get").mockReturnValue(8);
             const resultado = obtenerEstadoPartida()
             expect(resultado).toBe(estadoEsperado);
         })
-        it("debería devolver TE_HAS_PASADO puntuación === 7.5", () => {
+        it("debería devolver TE_HAS_PASADO si puntuación === 7.5", () => {
             const estadoEsperado: EstadoPartida = 'HAS_ACERTADO';
             vi.spyOn(partida, 'puntuacion', "get").mockReturnValue(7.5);
             const resultado = obtenerEstadoPartida()
@@ -27,13 +27,13 @@ describe('./src/motor.ts', () => {
         })
     })
     describe('obtenerCarta', () => {
-        it("debería sumar +2 si la carta >7", () => {
+        it("debería sumar +2 si la carta > 7", () => {
             const cartaAleatoria = 8;
             const resultadoEsperado = cartaAleatoria + 2;
             const resultado = obtenerCarta(cartaAleatoria);
             expect(resultadoEsperado).toBe(resultado);
         })
-        it("debería delvolver el valor de la carta si valor <=7", () => {
+        it("debería delvolver el valor de la carta si valor <= 7", () => {
             const cartaAleatoria = 6;
             const resultadoEsperado = cartaAleatoria;
             const resultado = obtenerCarta(cartaAleatoria);
@@ -41,14 +41,14 @@ describe('./src/motor.ts', () => {
         })
     })
     describe('puntosPorCarta', () => {
-        it("si numeroCarta>7 debería devolver 0.5.", () => {
+        it("debería devolver 0.5 si numeroCarta > 7", () => {
             const numeroCarta = 12;
             const resultadoEsperado = 0.5;
             const resultado = puntosPorCarta(numeroCarta);
             expect(resultado).toBe(resultadoEsperado);
 
         })
-        it("si numeroCarta<7, debería devolver valorCarta=numeroCarta", () => {
+        it("debería devolver valorCarta = numeroCarta si numeroCarta < 7", () => {
             const numeroCarta = 1;
             const resultadoEsperado = numeroCarta;
             const resultado = puntosPorCarta(numeroCarta);
